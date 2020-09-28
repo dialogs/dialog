f0e41737c168a314714c7384f4f2a1d251dea1d6
Artiom <artiomponomariovcode@gmail.com>
2020-09-28 20:20:24 +0300

    images reload, anchor fix, minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -574,7 +574,6 @@ gulp.task('serverbs', function () {
   gulp.watch(`${SOURCE_PATH}js/**/*.js`, gulp.series('js', 'refresh'));
 });
 
-
 gulp.task('refresh', function (done) {
   server.reload();
   done();
